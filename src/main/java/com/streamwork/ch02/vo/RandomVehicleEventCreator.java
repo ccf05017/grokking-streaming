f9c7f2c53195b5e93f654b195a97d02de8e5ec80
poppo <poppo@woowahan.com>
2023-12-30 14:49:41 +0900

    가독성을 높이기 위한 코드 리팩토링
    - 객체 간 책임을 명확하게 하기 위해 이름이 아닌 VehicleEvent를 직접 생성하도록 변경
    - 가독성을 높이기 위해 메서드 분리

--- src/main/java/com/streamwork/ch02/vo/RandomVehicleEventCreator.java
+++ src/main/java/com/streamwork/ch02/vo/RandomVehicleEventCreator.java
@@ -1,14 +1,16 @@
 package com.streamwork.ch02.vo;
 
+import com.streamwork.ch02.job.VehicleEvent;
+
 import java.util.List;
 import java.util.Random;
 
-public class RandomVehicleNameCreator {
+public class RandomVehicleEventCreator {
     private final List<String> carCollection = List.of("excavator", "truck", "truck-mixer", "car", "SUV");
 
-    public String create() {
+    public VehicleEvent create() {
         Random rand = new Random();
         int randomIndex = rand.nextInt(this.carCollection.size());
-        return carCollection.get(randomIndex);
+        return new VehicleEvent(carCollection.get(randomIndex));
     }
 }
